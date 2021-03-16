b033883581267ccc43d876f02e00c425c2aaafdf
Arnab Roy <arnabroy@chegg.com>
2021-03-17 03:43:09 +0530

    api key env fix

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    version: 'v1.0.9',
+    version: 'v1.0.10',
     appName: 'covid-india-stats',
     covidDataApi: 'https://corona-virus-world-and-india-data.p.rapidapi.com',
     apiKey: process.env.REACT_APP_COVID_API_KEY,
